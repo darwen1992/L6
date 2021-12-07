437e2ce98b8cb2099d66faa0c7c171f54772dd72
darwinmwstafa <darwin.mustafa92@gmail.com>
2021-12-07 15:16:18 +0100

    first version

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        int []x={7,2,9,4,5,6,1,8,3};
+        System.out.println("Main array ");
+        for(int name: x) {
+            System.out.print(name+" " );
+        }
+
+    }
+}
