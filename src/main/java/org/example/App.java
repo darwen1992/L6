c25ce22754ed90afe43eadc6c59c9ce33ff1265e
darwinmwstafa <darwin.mustafa92@gmail.com>
2021-12-07 15:17:33 +0100

    second version

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,6 +11,69 @@ public class App
         for(int name: x) {
             System.out.print(name+" " );
         }
+        System.out.println();
+        System.out.println("__________________________ ");
 
+        findNumber( x);
+        System.out.println("__________________________ ");
+
+        evenNumber( x );
+        System.out.println("__________________________ ");
+
+        sum( x);
+        System.out.println("__________________________ ");
+
+        revers( x );
+        System.out.println("__________________________ ");
+
+        sort( x );
+        System.out.println("__________________________ ");
+    }
+
+
+   static void findNumber( int[] x )
+    {
+        int indexFound = Arrays.binarySearch(x,5);
+        System.out.println("index of the number is "+ indexFound);
+    }
+
+    static void evenNumber( int[] x )
+    {
+        for (int i=0; i< x.length;i++){
+           if(x[i]%2==0){
+               System.out.print( x[i]+" " );
+           }
+        }
+        System.out.println();
+    }
+
+    static void sum( int[] x )
+    {
+        int sum=0;
+        for (int i=0; i< x.length;i++){
+            sum=sum+x[i];
+
+            }
+
+        System.out.print("sum= " +sum);
+        System.out.println();
+    }
+
+    static void revers( int[] x )
+    {
+
+        for (int i=x.length-1; i>=0;i--){
+            System.out.print(x[i]+" ");
+        }
+        System.out.println();
+    }
+
+    static void sort( int[] x )
+    {
+        Arrays.sort(x);
+        for(int i: x) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 }
